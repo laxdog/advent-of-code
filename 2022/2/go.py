b330999b21abcf3b3b94d545e1a1e44de471cc2f
Michael Robinson <laxdog@gmail.com>
2022-12-02 10:20:28 +0000

    Reduce lines, prep for future use of samples

--- 2022/2/go.py
+++ 2022/2/go.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
-
 def get_lines(file_type=None):
-    if file_type == '1':
-        return parse_file('sample1')
-    if file_type == '2':
-        return parse_file('sample2')
+    if file_type == 1: return parse_file('sample1')
+    if file_type == 2: return parse_file('sample2')
     return parse_file('input.txt')
 
 def parse_file(file):
     with open(file, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-def go():
+def go(file_type=None):
     score, score2 = 0, 0
-    for round in get_lines():
+    for round in get_lines(file_type):
         opp, me = round.split(' ')
-        opp = ord(opp) - 64
-        me = ord(me) - 87
+        opp = ord(opp) - ord('A') + 1
+        me = ord(me) - ord('X') + 1
         score += me
-        if opp == me:
-            score +=3
-        if (opp == 1 and me == 2) or (opp == 2 and me == 3) or(opp == 3 and me == 1):
-            score +=6
+        if opp == me: score += 3
+        if (opp - me) % 3 == 2: score += 6
         # Part 2
         if me == 1:
-            score2 += 0
             if opp == 1: score2 += 3
             else: score2 += (opp - 1)
-        if me == 2:
-            score2 += 3
-            score2 += opp
+        if me == 2: score2 += 3 + opp
         if me == 3:
             score2 += 6
             if opp == 3: score2 += 1
@@ -41,4 +32,5 @@ def go():
     print(score2)
 
 
+go(1)
 go()
