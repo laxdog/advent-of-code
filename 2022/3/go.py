36bb90bc38d810ac982888eabd17de24bcd57114
Michael Robinson <laxdog@gmail.com>
2022-12-03 05:35:13 +0000

    Tidy up

--- 2022/3/go.py
+++ 2022/3/go.py
@@ -9,39 +9,34 @@ def parse_file(file):
     with open(file, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
+def priority(item):
+    if item.islower():
+        return ord(item) - ord('a') + 1
+    else:
+        return ord(item) - ord('A') + 27
+
 def go(file_type=None):
-    sum = 0
-    for round in get_lines(file_type):
-        x = int(len(round)/2)
-        f, s = round[:x], round[x:]
-        print(f, s)
+    total = 0
+    for sack in get_lines(file_type):
+        x = len(sack)/2
+        f, s = sack[:x], sack[x:]
         common = (list(set(f) & set(s)))
         for item in common:
-            print(item)
-            if item.islower():
-                sum += ord(item) - ord('a') + 1
-            else:
-                sum += ord(item) - ord('A') + 27
+            total += priority(item)
+    print(total)
 
-    print(sum)
-
-    sackC = 0
-    sum = 0
+    count, total = 0, 0
     sacks = []
-    for round in get_lines(file_type):
-        sackC += 1
-        sacks.append(round)
-        if sackC == 3:
+    for sack in get_lines(file_type):
+        count += 1
+        sacks.append(sack)
+        if count == 3:
             common = (list(set(sacks[0]) & set(sacks[1]) & set(sacks[2])))
-            print(common, sacks)
-            sackC = 0
+            count = 0
             sacks = []
             for item in common:
-                if item.islower():
-                    sum += ord(item) - ord('a') + 1
-                else:
-                    sum += ord(item) - ord('A') + 27
+                total += priority(item)
+    print(total)
 
-        print(sum)
-go(1)
+# go(1)
 go()
