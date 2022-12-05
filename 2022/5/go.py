eb8f1e173d9182d26597554ce020d3bae9a8b6a4
Michael Robinson <laxdog@gmail.com>
2022-12-05 18:51:22 +0000

    Clean up day 5

--- 2022/5/go.py
+++ 2022/5/go.py
@@ -11,29 +11,19 @@ def parse_file(file):
     with open(file, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-start = """[P]     [C]         [M]            
-[D]     [P] [B]     [V] [S]        
-[Q] [V] [R] [V]     [G] [B]        
-[R] [W] [G] [J]     [T] [M]     [V]
-[V] [Q] [Q] [F] [C] [N] [V]     [W]
-[B] [Z] [Z] [H] [L] [P] [L] [J] [N]
-[H] [D] [L] [D] [W] [R] [R] [P] [C]
-[F] [L] [H] [R] [Z] [J] [J] [D] [D]"""
+start = ""
+for line in get_lines():
+    if line.startswith('['):
+        start += line + '                \n'
 
-
-s1, s2, s3, s4, s5, s6, s7, s8, s9 = [], [], [], [], [], [], [], [], []
-alls1 = [s1, s2, s3, s4, s5, s6, s7, s8, s9]
+alls1 = []
+for stack in range(9):
+    alls1.append([])
 
 for row in start.split('\n'):
-    if row[1] != ' ': s1.append(row[1])
-    if row[5] != ' ': s2.append(row[5])
-    if row[9] != ' ': s3.append(row[9])
-    if row[13] != ' ': s4.append(row[13])
-    if row[17] != ' ': s5.append(row[17])
-    if row[21] != ' ': s6.append(row[21])
-    if row[25] != ' ': s7.append(row[25])
-    if row[29] != ' ': s8.append(row[29])
-    if row[33] != ' ': s9.append(row[33])
+    if row == '': break
+    for char in range(1, 34, 4):
+        if row[char] != ' ': alls1[char//4].append(row[char])
 
 alls2 = deepcopy(alls1)
 
@@ -47,14 +37,8 @@ def go(file_type=None):
                 alls1[toS].insert(0, alls1[fromS].pop(0))
                 alls2[toS].insert(0, alls2[fromS].pop(num - move - 1))
 
-    out = ""
-    for s in alls1:
-        out += s[0]
-    print(out)
-    out = ""
-    for s in alls2:
-        out += s[0]
-    print(out)
+    print("".join([x[0] for x in alls1]))
+    print("".join([x[0] for x in alls2]))
 
 go()
 # go()
